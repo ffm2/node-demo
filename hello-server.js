10b53841f7987f6f8b782778d69a923402b9388d
Fardos Mohamed <ffm2@uw.edu>
2014-11-24 12:27:55 -0800

    all changes

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,17 @@
 
 'use strict';
 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.get('/foo', function(req,res){
+   res.send('You requested foo!');
+});
+
+app.listen(8080, function(){
+    console.log('Listening at http://localhost:8080');
+
+});
